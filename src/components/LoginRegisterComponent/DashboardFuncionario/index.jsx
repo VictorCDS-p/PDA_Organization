9692da7e3afc1e17f4ad40acb051108f843fcd9a
VictorCDS-p <Victor90774@outlook.com>
2024-10-03 15:51:58 -0300

    fix var names2

--- src/components/LoginRegisterComponent/DashboardFuncionario/index.jsx
+++ src/components/LoginRegisterComponent/DashboardFuncionario/index.jsx
@@ -9,7 +9,7 @@ export default function DashboardFuncionario() {
 
   useEffect(() => {
     const users = JSON.parse(localStorage.getItem("users")) || [];
-    const alunosReg = users.filter(user => user.user_type === "aluno");
+    const alunosReg = users.filter(user => user.tipo_usuario === "aluno");
     setAlunos(alunosReg);
   }, []);
 
@@ -33,7 +33,7 @@ export default function DashboardFuncionario() {
     return aluno.status === statusFilter;
   });
 
-  if (!user || user.user_type !== "funcionario") {
+  if (!user || user.tipo_usuario !== "funcionario") {
     return (
       <Container className="mt-5">
         <h2>Você não está autenticado como funcionário.</h2>
