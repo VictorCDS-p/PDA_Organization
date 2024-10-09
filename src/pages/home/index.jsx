0140b6789f7594e521b2b0df961f1ccaa10246ac
samira ferreira <samiraferreiramg03@gmail.com>
2024-10-08 22:44:05 -0300

    feat: committing the sectionHome component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/header';
+import SectionHome from '../../components/sectionHome/sectionHome';
 
 export default function Home() {
   return (
    <div>
     <Header/>
+    <SectionHome/>
    </div>
   );
 }
