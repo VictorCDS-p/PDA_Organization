b458d96d5b4d5f8f1ff5945e969ef28a9c1ea6cb
samira ferreira <samiraferreiramg03@gmail.com>
2024-10-08 21:22:57 -0300

    feat: trying to render the header

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthContext from './components/Context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
-import Header from './components/header';
-import AuthPage from './pages/AuthPage';  
-import EditAccount from './pages/EditAccount';
-import Manage from './pages/Manage';
-import Projects from './pages/Projects';
-import Success from './pages/Success';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContext>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/auth" element={<AuthPage />} /> 
-          <Route path="/edit-account" element={<EditAccount />} />
-          <Route path="/manage" element={<Manage />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/success" element={<Success />} />
-        </Routes>
-      </Router>
-    </AuthContext>
-  </React.StrictMode>
+    <BrowserRouter>
+    <Home/>
+    </BrowserRouter>
+   </React.StrictMode>
 );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
+import Header from '../../components/header';
 
 export default function Home() {
   return (
-    <div className="home-page">
-      <h1>Bem-vindo ao Sistema de Gerenciamento</h1>
-      <p>
-        Esta é a página inicial da nossa plataforma. Aqui, os usuários podem se 
-        registrar ou fazer login para acessar suas contas. O sistema permite que 
-        alunos e funcionários gerenciem turmas, projetos e suas informações pessoais.
-        <br />
-        <strong>Funcionalidade:</strong> 
-        - Um link para login e registro, que leva os usuários para a página de autenticação.
-        - Uma breve descrição do que a plataforma oferece.
-      </p>
-    </div>
+   <div>
+    <Header/>
+   </div>
   );
 }
 
