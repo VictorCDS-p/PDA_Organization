15fde22299d18c0ce3ceb1f26f90556df7c13600
samira ferreira <samiraferreiramg03@gmail.com>
2024-10-08 21:47:59 -0300

    BugFix: solving navbar rendering bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
+import AuthProvider from './components/Context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
     <Home/>
     </BrowserRouter>
+    </AuthProvider>
    </React.StrictMode>
 );
