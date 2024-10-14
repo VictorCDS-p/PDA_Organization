2fd2679b41b8df16c6393dbb08121cee23923f65
Leticia Monteiro <leticiamonteiro0712@gmail.com>
2024-10-14 04:52:58 -0300

    update home page

--- src/components/sectionHome/sectionHome.jsx
+++ src/components/sectionHome/sectionHome.jsx
@@ -5,15 +5,15 @@ import './sectionHome.css';
 const SectionHome = () => {
   return (
     <div className="section-home">
-      <Image
+      {/* <Image
         className="image-pda"
         src="images/imagem.png"
         alt="Programadores do Amanhã"
         fluid
-      />
+      /> */}
       <Container className="texto-sobreposto">
         <h1>O Instituto Programadores do Amanhã</h1>
-        <p>
+        <p id="subtitle-home">
           Nasceu com o objetivo de formar e empregar jovens pretos, pardos e
           indígenas no mercado de tecnologia.
         </p>
